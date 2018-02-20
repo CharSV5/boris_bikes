46128f92d69c3ccb6d005bc87c9a6f8bd416d7ea
Charlene Bastians <charlenebastians@hotmail.com>
2018-02-20 09:32:58 -0800

    Bike class/rspec

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,18 @@
 class DockingStation
 
   def release_bike
+    Bike.new
+  end
+
+end
+
+class Bike
+
+  def working?
+    puts "It is working"
   end
 
 end
 
 docking_station = DockingStation.new
+bike = docking_station.release_bike.working?
---
+++ spec/bike_spec.rb
@@ -0,0 +1,8 @@
+require "docking_station"
+
+describe  Bike do
+
+    describe "Bike is working?" do
+      it { should respond_to(:working?)}
+    end
+end
