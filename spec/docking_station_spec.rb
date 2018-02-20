9056ca0fbd235fa62f042726b7344057732e573b
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-20 17:11:03 +0000

    Added test in one-liner format

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,15 @@ require "docking_station"
 
 describe DockingStation do
 
-  it 'release bike from docking station' do
-    expect(subject).to respond_to(:release_bike)
-  end
+   describe 'release_bike' do
+    it 'release bike from docking station' do
+      expect(subject).to respond_to(:release_bike)
+    end
+
+    ## One-liner
+    # describe 'releases bike from docking station' do
+    #   it { should respond_to(:release_bike) }
+    # end
+   end
 
 end
