e81f10b11564fa7687c5a46de7cac01ec44f4456
Charlene Bastians <charlenebastians@hotmail.com>
2018-02-21 10:09:18 -0800

    error messages and rspecs

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,24 @@
 # create new Docking Station class
 class DockingStation
-  attr_reader :bikes 
-  def initialize
+  attr_reader :bikes
+  def initialize(number_of_bikes = 0)
     @bikes = []
+    number_of_bikes.times {@bikes.push(Bike.new)}
   end
   def release_bike
-   Bike.new
+    if @bikes.empty?
+      raise "Sorry, there are no more bikes."
+    else
+      Bike.new
+    end
   end
   def dock_bike(bike)
-    @bikes.push(bike)
-  end
+    if @bikes.length == 1
+      raise "Sorry, there aren't any spaces to dock"
+    else
+      @bikes.push(bike)
+    end
+  end 
 end
 
 # create new Bike class
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,23 +1,24 @@
 require 'docking_station'
 describe DockingStation do
-  # describe 'release_bike' do
-  #   it 'release bike from docking station' do
-  #     expect(subject).to respond_to(:release_bike)
-  #   end
-
-    ## One-liner
-    describe 'releases bike from docking station' do
-      it { should respond_to(:release_bike) }
-    end
-    describe "gets bike, is bike working?" do
-      it { expect(DockingStation.new.release_bike).to respond_to(:working?)}
-    end
-  describe "docking bike" do
-    it { expect(DockingStation.new).to respond_to (:dock_bike)}
+  describe 'releases bike from docking station' do
+    it { should respond_to(:release_bike) }
   end
-  describe "can a user dock a bike" do
+  describe 'gets bike, is bike working?' do
+    station = DockingStation.new(1)
+    it { expect(station.release_bike).to respond_to (:working?) }
+  end
+  describe 'docking bike' do
+    it { expect(DockingStation.new).to respond_to :dock_bike }
+  end
+  describe 'can a user dock a bike ' do
     station = DockingStation.new
-    it { expect(station.dock_bike(station.release_bike)).to eq (station.bikes) }
+    it { expect(station.dock_bike(Bike.new)).to eq station.bikes }
   end
-
-end
+  describe 'no more bikes error message' do
+    it { expect {DockingStation.new.release_bike}.to raise_error("Sorry, there are no more bikes.") }
+  end
+  describe 'cannot except more bikes' do
+    station = DockingStation.new(1)
+    it {expect {station.dock_bike(Bike.new)}.to raise_error("Sorry, there aren't any spaces to dock") }
+  end
+end 
